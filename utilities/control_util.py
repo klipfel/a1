a0a87f1b04a0741d8d530d497a22acce593b16d3
arnaud <arnaudklipfel@hotmail.com>
2022-01-13 01:39:18 -0500

    Added path for the motion imitation wrapper on hdw.

--- utilities/control_util.py
+++ utilities/control_util.py
@@ -11,7 +11,11 @@ import pybullet  # pytype:disable=import-error
 import pybullet_data
 from pybullet_utils import bullet_client
 
+USER = os.system("echo $USER")
+
 # Motion imitation wrapper
+if USER == "unitree":  # adds the path to the local motion_imitation wrapper installation.
+    os.sys.path.append("/home/unitree/arnaud/motion_imitation")
 from motion_imitation.robots import robot_config
 
 from utilities.config import Config
